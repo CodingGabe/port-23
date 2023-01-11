ff151187084cd25faa645d0a3f92aa79ef901f4d
Gabe Amaya <gabeamaya2@gmail.com>
2023-01-11 15:51:48 -0600

    first commit

---
+++ app.vue
@@ -0,0 +1,17 @@
+<script>
+  import Navigation from "./components/Navigation.vue"
+  export default {
+    components: { Navigation }
+  }
+</script>
+
+<template>
+  <div>
+    <Navigation/>
+    <div class="w-full">
+      <div class="max-w-screen-lg w-full mt-16 mb-20 px-8 mx-auto pb-24 sm:mt-28 sm:px-16 slg:px-24 lg:mt-48 lg:mt-58">
+        <NuxtPage/>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    <h1>Crafting Beautiful &amp; Intuitive Web Experiences</h1>
+    <p class="text-sm lg:text-lg slg:leading-8 leading-relaxed">Howdy, my name is Gabe and I'm a Front-End Web Developer with a passion for creating dynamic and user-friendly websites and web applications. With strong skills in HTML, CSS, and JavaScript, I bring designs to life with stunning visuals and interactive features.</p>
+    <p class="text-sm lg:text-lg slg:leading-8 leading-relaxed">I also specialize in responsive design, ensuring that my work looks great and functions seamlessly on all devices. Take a look at my portfolio to see some of my recent projects and learn more about my expertise as a Front-End Web Developer.</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
