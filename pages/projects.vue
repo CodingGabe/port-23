94eeb3c5f6dd89f6288736a9d5f3ffd047c09eec
Gabe Amaya <gabeamaya2@gmail.com>
2023-01-12 11:01:25 -0600

    add icon to project button

--- pages/projects.vue
+++ pages/projects.vue
@@ -16,7 +16,7 @@
             <div class="flex flex-col items-left gap-6">
               <h3 class="text-xl sm:text-2xl md:text-4xl">Vue Shopping App</h3>
               <p class="text-md lg:text-lg slg:leading-8 leading-relaxed">A simple shopping app built with Vue JS. I learned the basics on how to filter through json files data to load in content along with components, router and the fundamentals of Options API.</p>
-              <NuxtLink href="https://gabes-shopping-list.netlify.app" class="px-2 py-2 rounded-md transition-colors border-[1px] w-fit text-sm md:px-4 slg:px-6 bg-teal-500 border-black hover:bg-teal-700">View App</NuxtLink>
+              <NuxtLink href="https://gabes-shopping-list.netlify.app" class="bg-zinc-900 hover:bg-zinc-700 px-2 py-2 rounded-sm w-fit md:px-4 slg:px-6 slg:py3 transition-colors border-0 font-bold">View App <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-up-right inline"><line x1="7" y1="17" x2="17" y2="7"></line><polyline points="7 7 17 7 17 17"></polyline></svg></NuxtLink>
             </div>
           </div>
           <!-- Project -->
@@ -27,7 +27,7 @@
             <div class="flex flex-col items-left gap-6">
               <h3 class="text-xl sm:text-2xl md:text-4xl">Zarya Creative Studio</h3>
               <p class="text-md lg:text-lg slg:leading-8 leading-relaxed">A beautiful website crafted by me in Webflow.  Main goal was to really flex my design skills and think outside of the norm. This agency is for small business' to hire my friend and I for creative services at an affordable rate.</p>
-              <NuxtLink href="https://gabes-shopping-list.netlify.app" class="px-2 py-2 rounded-md transition-colors border-[1px] w-fit text-sm md:px-4 slg:px-6 bg-teal-500 border-black hover:bg-teal-700">View Site</NuxtLink>
+              <NuxtLink href="https://gabes-shopping-list.netlify.app" class="bg-zinc-900 hover:bg-zinc-700 px-2 py-2 rounded-sm w-fit md:px-4 slg:px-6 slg:py3 transition-colors border-0 font-bold">View Site <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-up-right inline"><line x1="7" y1="17" x2="17" y2="7"></line><polyline points="7 7 17 7 17 17"></polyline></svg></NuxtLink>
             </div>
           </div>
         </div>
