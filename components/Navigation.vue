4fb669fb53f21748e2d83a99b54587fc0a3b819e
Gabe Amaya <GabeAmaya2@gmail.com>
2023-03-02 11:11:11 -0600

    fix desktop layout for shop

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -3,15 +3,16 @@
         <!-- Navigation -->
         <nav class="px-3 py-4 flex justify-between items-center fixed w-full backdrop-blur-sm sm:top-0 sm:bottom-auto sm:px-6 lg:py-4 border-b-1 border-nav--color bottom-0 bg-na">
             <div class="top-0 absolute left-0 w-full bg-topbar"></div>
-            <NuxtLink to="/"><span class="rounded-md transition-all"><img src="../assets/img/y2k-gabe_light.png" alt="Gabe Logo" class="object-cover max-w-[4rem] h-auto"></span></NuxtLink>
-            <div class="flex items-center gap-1 sm:gap-2">
-                <NuxtLink to="/about"><span
-                    class="px-3 py-1 pb-2 pt-2 rounded-md transition-all sm:px-6 sm:py-2">About</span></NuxtLink>
-                <NuxtLink to="/projects"><span
-                    class="px-3 py-1 pb-2 pt-2 rounded-md transition-all sm:px-6 sm:py-2">Projects</span></NuxtLink>
-                <NuxtLink to="/shop"><span
-                    class="px-3 py-1 pb-2 pt-2 rounded-md transition-all sm:px-6 sm:py-2">Shop</span></NuxtLink>
-            </div>
+                <NuxtLink to="/"><span class="rounded-md transition-all"><img src="../assets/img/y2k-gabe_light.png" alt="Gabe Logo" class="object-cover max-w-[4rem] h-auto"></span></NuxtLink>
+                <div class="flex items-center gap-1 sm:gap-2">
+                    <NuxtLink to="/about"><span
+                        class="px-3 py-1 pb-2 pt-2 rounded-md transition-all sm:px-6 sm:py-2">About</span></NuxtLink>
+                    <NuxtLink to="/projects"><span
+                        class="px-3 py-1 pb-2 pt-2 rounded-md transition-all sm:px-6 sm:py-2">Projects</span></NuxtLink>
+                    <NuxtLink to="/shop"><span
+                        class="px-3 py-1 pb-2 pt-2 rounded-md transition-all sm:px-6 sm:py-2">Shop</span></NuxtLink>
+                </div>
+            
         </nav>
     </div>
 </template>
